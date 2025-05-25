9d64bb97d8994e55dc23e425fcac4b9a94af8656
Carlos Henrique <luiscarloslopes597@gmail.com>
2025-05-25 17:22:24 -0300

    Implementando a utilização da matriz de LEDs.

--- ControleDeAcessos.c
+++ ControleDeAcessos.c
@@ -52,22 +52,22 @@ SemaphoreHandle_t xCounterSem;
 
 ssd1306_t ssd;
 
-static uint32_t last_irq_time_joy = 0;
+static uint32_t lastIrqTime = 0; // Registra o tempo da ultima interrupcao
 
-// Flag para alternar o modo. True = modo normal, false = modo noturno.
-volatile bool modoNormalOn = true;
-volatile bool limiteAtingido = false;
-volatile bool botaoPressionado = false;
-volatile uint8_t numeroDeUsuarios = 0;
-volatile uint8_t ultimoBotaoPressionado = 0;
+volatile bool botaoPressionado = false;      // True: se um botao foi pressionado nos ultimos 1.25s. Senao, false
+volatile uint8_t numeroDeUsuarios = 0;       // Registra o numero de usuarios no sistema
+volatile uint8_t ultimoBotaoPressionado = 0; // Detecta o ultimo botao pressionado, A, B ou Joystick
 
+/*
+ * Processo para o botao do joystick (gpio 22). Reinicia o numero de usuarios a 0 e mostra no display a
+ * quantidade de foi retirada. A escrita no display, bem como para os outros botoes, eh protegida pelo
+ * mutex. Utiliza um semaforo binario para registrar a interrupcao.
+ */
 void vTaskReset()
 {
     static char buffer[13];
     while (true)
     {
-        // Tarefa está bloqueada até que o semáforo seja liberado
-        // O semáforo é liberado na ISR do botão
         if (xSemaphoreTake(xBinarySem, portMAX_DELAY) == pdTRUE)
         {
             if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
@@ -85,34 +85,13 @@ void vTaskReset()
     }
 }
 
-void vTaskSaida()
-{
-    while (true)
-    {
-        if (xSemaphoreTake(xCounterSem, portMAX_DELAY) == pdTRUE)
-        {
-            if (ultimoBotaoPressionado == BUTTON_B && numeroDeUsuarios != 0)
-            {
-                if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
-                {
-                    ssd1306_draw_string(&ssd, "-1 usuario!", 5, 5);
-                    ssd1306_send_data(&ssd);
-                    xSemaphoreGive(xMutex);
-                }
-                vTaskDelay(pdMS_TO_TICKS(1250));
-                numeroDeUsuarios--;
-                botaoPressionado = false;
-                printf("%d", numeroDeUsuarios);
-            }
-            else
-            {
-                // Devolve o semáforo se não for o botão B
-                xSemaphoreGive(xCounterSem);
-            }
-        }
-    }
-}
-
+/*
+ * Processo para o botao A (gpio 5). Adiciona um usuario no sistema e mostra "usuario adicionado!" no
+ * display. A escrita no display eh protegida pelo mutex. Utiliza o semaforo contador para registrar a
+ * interrupcao. Como o semaforo eh compartilhado com a tarefa de saida (botao B), ele verifica se
+ * realmente foi o botao A o ultimo pressionado (Give() feito pelo botao A), e se o numero de usuarios eh
+ * menor que 10. Se sim, prossegue com a remocao do usuario. Senao, devolve o semaforo.
+ */
 void vTaskEntrada()
 {
     while (true)
@@ -141,6 +120,49 @@ void vTaskEntrada()
     }
 }
 
+/*
+ * Processo para o botao B (gpio 6). Retira um usuario do sistema e mostra "-1 usuario!" no display.
+ * A escrita no display eh protegida pelo mutex. Utiliza o semaforo contador para registrar a interrupcao.
+ * Como o semaforo eh compartilhado com a tarefa de entrada (botao A), ele verifica se realmente foi o
+ * botao B o ultimo pressionado (Give() feito pelo botao B), e se o numero de usuarios eh maior que 0.
+ * Se sim, prossegue com a remocao do usuario. Senao, devolve o semaforo.
+ */
+void vTaskSaida()
+{
+    while (true)
+    {
+        if (xSemaphoreTake(xCounterSem, portMAX_DELAY) == pdTRUE)
+        {
+            if (ultimoBotaoPressionado == BUTTON_B && numeroDeUsuarios > 0)
+            {
+                if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+                {
+                    ssd1306_draw_string(&ssd, "-1 usuario!", 5, 5);
+                    ssd1306_send_data(&ssd);
+                    xSemaphoreGive(xMutex);
+                }
+                vTaskDelay(pdMS_TO_TICKS(1250));
+                numeroDeUsuarios--;
+                botaoPressionado = false;
+                printf("%d", numeroDeUsuarios);
+            }
+            else
+            {
+                // Devolve o semáforo se não for o botão B
+                xSemaphoreGive(xCounterSem);
+            }
+        }
+    }
+}
+
+void tocarBuzzer(uint slice, uint chan, uint wrap, float freq)
+{
+    pwm_set_clkdiv(slice, freq);
+    pwm_set_chan_level(slice, chan, wrap / 2);
+    vTaskDelay(pdMS_TO_TICKS(100));
+    pwm_set_chan_level(slice, chan, 0);
+}
+
 void vTaskBuzzer()
 {
     uint slice = pwm_gpio_to_slice_num(BUZZER_PIN);
@@ -153,44 +175,26 @@ void vTaskBuzzer()
     {
         if (botaoPressionado && (0 <= numeroDeUsuarios && numeroDeUsuarios < 8))
         {
-            pwm_set_clkdiv(slice, 2.0f);
-            pwm_set_chan_level(slice, chan, 0);
-            wrap = 125000000 / 3500;
-            pwm_set_chan_level(slice, chan, wrap / 2);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            pwm_set_chan_level(slice, chan, 0);
+            tocarBuzzer(slice, chan, 125000000 / 3500, 2.0f);
             wrap = 0;
-
             vTaskDelay(pdMS_TO_TICKS(1250));
             botaoPressionado = false;
         }
         else if (botaoPressionado && numeroDeUsuarios == 8)
         {
-            pwm_set_clkdiv(slice, 4.0f);
-            pwm_set_chan_level(slice, chan, 0);
-            wrap = 125000000 / 3500;
-            pwm_set_chan_level(slice, chan, wrap / 2);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            pwm_set_chan_level(slice, chan, 0);
+            tocarBuzzer(slice, chan, 125000000 / 3500, 4.0f);
             wrap = 0;
-
             vTaskDelay(pdMS_TO_TICKS(1250));
             botaoPressionado = false;
         }
         else if (botaoPressionado && numeroDeUsuarios == 9)
         {
-            pwm_set_clkdiv(slice, 8.0f);
-            pwm_set_chan_level(slice, chan, 0);
-            wrap = 125000000 / 3500;
-            pwm_set_chan_level(slice, chan, wrap / 2);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            pwm_set_chan_level(slice, chan, 0);
+            tocarBuzzer(slice, chan, 125000000 / 3500, 8.0f);
             wrap = 0;
-
             vTaskDelay(pdMS_TO_TICKS(1250));
             botaoPressionado = false;
         }
-        if (limiteAtingido)
+        if (botaoPressionado && numeroDeUsuarios == 10)
         {
             pwm_set_clkdiv(slice, 10.0f);
             pwm_set_chan_level(slice, chan, 0);
@@ -205,7 +209,6 @@ void vTaskBuzzer()
             vTaskDelay(pdMS_TO_TICKS(1250));
             wrap = 0;
 
-            limiteAtingido = false;
             botaoPressionado = false;
         }
         vTaskDelay(pdMS_TO_TICKS(10));
@@ -242,6 +245,97 @@ void vTaskRGB()
             pwm_set_gpio_level(LED_PIN_RED, 255);
         }
         vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+uint32_t matrix_rgb(double r, double g, double b)
+{
+    unsigned char R = r * 255;
+    unsigned char G = g * 255;
+    unsigned char B = b * 255;
+    return (G << 24) | (R << 16) | (B << 8);
+}
+
+// Envia padrão para LEDs WS2812
+void desenho_pio(double *desenho, PIO pio, uint sm, float r, float g, float b)
+{
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        pio_sm_put_blocking(pio, sm, matrix_rgb(desenho[24 - i] * r, desenho[24 - i] * g, desenho[24 - i] * b));
+    }
+}
+
+void vTaskMatriz()
+{
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &blink_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    blink_program_init(pio, sm, offset, WS2812_PIN);
+
+    double *feedback[4] = {novoUsuario, alerta, erro, matrizDesligada};
+
+    // Vetor com os numeros que serao exibidos na matriz. Todos eles estao desenhados em lib/numeros.c
+    double *animacaoReinicio[15] = {reinicio1, reinicio2, reinicio3, reinicio4, reinicio5, reinicio6,
+                                    reinicio7, reinicio8, reinicio9, reinicio10, reinicio11, reinicio12,
+                                    reinicio13, reinicio14, matrizDesligada};
+
+    while (true)
+    {
+        if (ultimoBotaoPressionado == BUTTON_JOY && botaoPressionado)
+        {
+            desenho_pio(animacaoReinicio[0], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[1], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[2], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[3], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[4], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[5], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[6], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[7], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[8], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[9], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[10], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[11], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[12], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[13], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+            desenho_pio(animacaoReinicio[14], pio, sm, 0.0, 1.0, 0.0);
+            vTaskDelay(pdMS_TO_TICKS(84));
+        }
+        else if (ultimoBotaoPressionado == BUTTON_A && botaoPressionado)
+        {
+            if (0 <= numeroDeUsuarios && numeroDeUsuarios < 8)
+            {
+                desenho_pio(feedback[0], pio, sm, 0.0, 0.0, 1.0);
+                vTaskDelay(pdMS_TO_TICKS(1250));
+            }
+            else if (numeroDeUsuarios == 9)
+            {
+                desenho_pio(feedback[0], pio, sm, 1.0, 1.0, 0.0);
+                vTaskDelay(pdMS_TO_TICKS(625));
+                desenho_pio(feedback[1], pio, sm, 1.0, 1.0, 0.0);
+                vTaskDelay(pdMS_TO_TICKS(625));
+            }
+            else if (numeroDeUsuarios == 10)
+            {
+                desenho_pio(feedback[0], pio, sm, 1.0, 0.0, 0.0);
+                vTaskDelay(pdMS_TO_TICKS(625));
+                desenho_pio(feedback[2], pio, sm, 1.0, 0.0, 0.0);
+                vTaskDelay(pdMS_TO_TICKS(625));
+            }
+        }
     }
 }
 
@@ -345,26 +439,17 @@ void gpio_irq_handler(uint gpio, uint32_t events)
         return;
 
     uint32_t now = to_ms_since_boot(get_absolute_time());
-    if (now - last_irq_time_joy < DEBOUNCE_MS)
+    if (now - lastIrqTime < DEBOUNCE_MS)
         return;
-    last_irq_time_joy = now;
+    lastIrqTime = now;
 
     if (gpio == BUTTON_A)
     {
         botaoPressionado = true;
 
-        if (numeroDeUsuarios == 10)
-        {
-            // chegamos ao limite: só tocamos o buzzer
-            limiteAtingido = true;
-            // **não** chamar gpio_callback nem dar mais um give no xCounterSem
-        }
-        else
-        {
-            // caso normal: libera o counting semaphore para vTaskEntrada
-            ultimoBotaoPressionado = BUTTON_A;
-            gpio_callback(gpio, events);
-        }
+        // caso normal: libera o counting semaphore para vTaskEntrada
+        ultimoBotaoPressionado = BUTTON_A;
+        gpio_callback(gpio, events);
     }
     else if (gpio == BUTTON_B)
     {
@@ -439,6 +524,8 @@ int main()
     xTaskCreate(vTaskReset, "Tarefa para entrada do usuario", configMINIMAL_STACK_SIZE,
                 NULL, tskIDLE_PRIORITY, NULL);
     xTaskCreate(vTaskRGB, "Tarefa dos LEDs", configMINIMAL_STACK_SIZE,
+                NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vTaskMatriz, "Tarefa para entrada do usuario", configMINIMAL_STACK_SIZE,
                 NULL, tskIDLE_PRIORITY, NULL);
     xTaskCreate(vTaskDisplay, "Tarefa dos LEDs", configMINIMAL_STACK_SIZE,
                 NULL, tskIDLE_PRIORITY, NULL);
